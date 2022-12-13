568a5dd7bf9efe18e84b6e0113757a34314de54b
leehyeseong0821 <lhs5567@naver.com>
2022-12-13 21:48:50 +0900

    spring boot

--- src/main/java/com/study/springboot202212lhs/web/controller/Controller1.java
+++ src/main/java/com/study/springboot202212lhs/web/controller/Controller1.java
@@ -2,17 +2,24 @@ package com.study.springboot202212lhs.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class Controller1 {
 
     //@RequestMapping(value = "/page1", method =RequestMapping.GET)
     @GetMapping("/page1")
-    public String page1(){
-
-        return "page1";
+    public ModelAndView page1(){
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("page1");
+        mav.addObject("key","value");
+        return mav;
     }
 
+    @GetMapping("/page2")
+    public String page2(){
 
+        return "page2";
+    }
 
 }
