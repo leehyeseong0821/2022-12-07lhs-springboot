2ca9d1d0a3022a6e770f87b7d1192468d5407b16
leehyeseong0821 <lhs5567@naver.com>
2022-12-19 21:55:05 +0900

    spring db연결

--- src/main/java/com/study/springboot202212lhs/repository/UserRepository.java
+++ src/main/java/com/study/springboot202212lhs/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package com.study.springboot202212lhs.repository;
 
 import com.study.springboot202212lhs.dto.UserDto;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface UserRepository {
 
     public int saveUser(UserDto userDto);
